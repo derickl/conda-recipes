b36132f85770fa67470bbdbf85f8527769d0bf33
Derick Lung'aho <derick.lungaho@gmail.com>
2014-12-29 07:13:24 +0300

    Fix qwt recipe

--- qwt/build.sh
+++ qwt/build.sh
@@ -1,4 +1,4 @@
-gsed -i 's/^\s*QWT_INSTALL_PREFIX\s*=\(.*\)$/QWT_INSTALL_PREFIX=$PREFIX/' qwtconfig.pri
+gsed -i "s|^\s*QWT_INSTALL_PREFIX\s*=\(.*\)$|QWT_INSTALL_PREFIX=$PREFIX|" qwtconfig.pri
 gsed -i 's/^\s*QWT_CONFIG\s*\+=\s*QwtDesigner$//' qwtconfig.pri
 
 touch $PREFIX/bin/qt.conf
@@ -8,13 +8,7 @@ echo 'Prefix = ../' >> $PREFIX/bin/qt.conf
 
 cp -R $PREFIX/mkspecs $PREFIX/bin
 
-
-qmake -query
-
-#export QMAKESPEC=$PREFIX/mkspecs/unsupported/macx-clang-libc++
-
-
-qmake -config release -spec macx-g++
+qmake -config release -spec unsupported/macx-clang-libc++
 
 make
 make install
