4128db7b0f6288a6076d8a57404ccd9904485ce1
Derick Lung'aho <derick.lungaho@gmail.com>
2015-01-24 14:29:06 +0300

    update graphlab 1.2.1 recipe

--- graphlab-create/build.sh
+++ graphlab-create/build.sh
@@ -4,6 +4,10 @@ $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
 
+for file in $PREFIX/lib/python$PY_VER/site-packages/graphlab/cython/cy*.so
+do
+  install_name_tool -change @rpath/libbase_dep.so @loader_path/libbase_dep.so $file
+done
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
