86da3e38952e45f6dec424940074da937a947f02
Derick Lung'aho <derick.lungaho@gmail.com>
2015-02-01 18:02:03 +0300

    Updates, new recipes

--- disco/build.sh
+++ disco/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
+
 gsed -i "s:sysconfdir    = /etc:sysconfdir = $PREFIX/etc:g" Makefile
 gsed -i "s:prefix        = /usr:prefix = $PREFIX:g" Makefile
 
 make
 make install
-
 gsed -i "s:/etc/disco/:$PREFIX/etc/disco/:g" \
     $PREFIX/lib/python2.7/site-packages/disco/settings.py
+    
+cd contrib/discodb
+
+$PYTHON setup.py install --prefix=$PREFIX
