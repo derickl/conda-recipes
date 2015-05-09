110c25c49b773f1dc5de98889da396539a0054d6
Derick Lung'aho <derick.lungaho@gmail.com>
2015-05-09 17:53:42 +0300

    updates

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -12,6 +12,7 @@ export LDFLAGS="-L${PREFIX}/lib"
 
 chmod +x configure;
 
+<<<<<<< HEAD
 ./configure \
     --enable-gpl \
     --enable-nonfree \
@@ -23,3 +24,36 @@ chmod +x configure;
     --prefix=$PREFIX
 make 
 make install
+=======
+DarwinInstallation() {
+
+    chmod +x configure;
+
+    ./configure \
+        --enable-gpl \
+        --enable-nonfree \
+        --enable-shared \
+        --enable-pic \
+        --enable-libx264 \
+        --enable-openssl \
+        --disable-podpages \
+        --prefix=${PREFIX} || return 1;
+    make || return 1;
+    make install || return 1;
+
+    return 0;
+}
+
+case ${ARCH} in
+    'Linux')
+        LinuxInstallation || exit 1;
+        ;;
+    'Darwin')
+        DarwinInstallation || exit 1;
+	;;
+    *)
+        echo -e "Unsupported machine type: ${ARCH}";
+        exit 1;
+        ;;
+esac
+>>>>>>> 9e53383c97afa9454bf4ae59cf7891224e8bdfca
