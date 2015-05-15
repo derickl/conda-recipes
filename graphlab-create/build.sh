3a64c6aa2df8d2df2efeb9a5e4464f51a760672b
Derick Lung'aho <derick.lungaho@gmail.com>
2015-05-15 20:31:52 +0300

    updates

--- graphlab-create/build.sh
+++ graphlab-create/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf grahlab/linux
+
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
@@ -8,6 +10,11 @@ for file in $PREFIX/lib/python$PY_VER/site-packages/graphlab/cython/cy*.so
 do
   install_name_tool -change @rpath/libbase_dep.so @loader_path/libbase_dep.so $file
 done
+
+rm -rf $PREFIX/lib/python$PY_VER/site-packages/graphlab/osx
+rm -rf $PREFIX/lib/python$PY_VER/site-packages/graphlab/linux
+
+
 # See
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
