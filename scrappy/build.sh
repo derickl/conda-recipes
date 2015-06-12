78fc68d62cfc98feea6a83e76001826341e64a8e
Derick Lung'aho <derick.lungaho@gmail.com>
2015-06-12 17:49:36 +0300

    updates

--- scrappy/build.sh
+++ scrappy/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-${PYTHON} setup.py install
+touch requirements.txt;
+
+${PYTHON} setup.py install || exit 1;
+
+#POST_LINK=${PREFIX}/bin/.scrappy-post-link.sh
+#cp ${RECIPE_DIR}/post-link.sh ${POST_LINK}
+#chmod +x ${POST_LINK}
